a7aab64f236fe628ff42bdbd9ba3c1156cd12cf8
mohsen-ramshini <rsmohsen20@gmail.com>
2025-05-13 21:39:25 +0330

    build error fixed 10

--- postcss.config.mjs
+++
@@ -1,7 +0,0 @@
-// postcss.config.mjs
-export default {
-  plugins: [
-    require('@tailwindcss/postcss'),  // به جای رشته، از require برای بارگذاری ماژول استفاده کن
-    require('autoprefixer'),
-  ],
-};
